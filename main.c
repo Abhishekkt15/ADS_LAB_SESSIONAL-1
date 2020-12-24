8f60d38b6e303c959697d6328c7814a1edfb1104
Abhishek K T <39241612+Abhishekkt15@users.noreply.github.com>
2020-12-24 10:33:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "bank.h"
 
+
+void test(){
+
+    Slist s1 = slist_new();
+    Slist *list= &s1;
+    assert (count_cust(list) == 0);
+
+    list = open_acc(list,"Pramod","",26,"CSIPA1473A","SB","9865478523",2000);
+    assert(list->head->data.initial_depo == 2000);
+    list = open_acc(list,"Dileep","",24,"TYSUW4562J","SB","9865478523",3000);
+    assert (list->head->data.age == 24);
+    
+
+
+}
+
+
+int main()
+{
+    test();
+    return 0;
+}
